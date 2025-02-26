69bce49a5e7e5d4a8a92a9ed6117c701a4d8686a
akara286 <101600331+akara286@users.noreply.github.com>
2025-02-26 14:17:40 -0700

    maybe working multipeer

--- XR Anatomy Vision/XRAnatomy_visionOSApp.swift
+++ XR Anatomy Vision/XRAnatomy_visionOSApp.swift
@@ -3,16 +3,20 @@ import SwiftUI
 @main
 struct XRAnatomy_visionOSApp: App {
     @StateObject private var appModel = AppModel()
+    @StateObject private var arViewModel = ARViewModel()  // Create ARViewModel
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(appModel)
+                .environmentObject(arViewModel)  // Inject ARViewModel here
         }
         .windowStyle(.volumetric)
         
         ImmersiveSpace(id: appModel.immersiveSpaceID) {
             ImmersiveView()
                 .environmentObject(appModel)
+                .environmentObject(arViewModel)  // Also inject in immersive view
                 .onAppear { appModel.immersiveSpaceState = .open }
                 .onDisappear { appModel.immersiveSpaceState = .closed }
         }
