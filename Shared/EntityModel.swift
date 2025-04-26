4a23d4531c04da95bc0190c2da7ab0e06e9bf646
Joanna235 <joannali896@gmail.com>
2025-04-25 21:25:50 -0600

    UI and model selection information

---
+++ Shared/EntityModel.swift
@@ -0,0 +1,8 @@
+//
+//  EntityModel.swift
+//  XR Anatomy
+//
+//  Created by Joanna  Lin  on 2025-04-22.
+//
+
+import Foundation
---
+++ XR Anatomy Vision/Scenes/ModelEntityHelper.swift
@@ -0,0 +1,8 @@
+//
+//  ModelEntityHelper.swift
+//  XR Anatomy Vision
+//
+//  Created by Joanna  Lin  on 2025-04-21.
+//
+
+import Foundation
